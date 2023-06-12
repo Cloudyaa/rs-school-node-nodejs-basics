50c2b800c27d0e9ef1cb5e5e9a2641e5f3e338cb
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-12 22:04:16 +0100

    Implement function that copies folder with all its content

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,52 @@
+//implement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
+
+import {copyFile, mkdir, readdir} from 'node:fs/promises';
+import {join, dirname} from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
-};
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+    const filesDirPath = join(__dirname, 'files');
+    const filesCopyDirPath = join(__dirname, 'files_copy')
+
+    let copyExists = false;
+    let filesToCopy;
+
+    // check if files folder exists
+    try {
+      filesToCopy = await readdir(filesDirPath);
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      }
+    }
+
+    // check if files_copy folder exists
+    try {
+      await readdir(filesCopyDirPath);
+      copyExists = true;
+    } catch (e) {
+      // if files_copy does not exist, create it and copy files
+      if (e.code === 'ENOENT') {
+        await mkdir(filesCopyDirPath);
+        await readdir(filesDirPath);
+        for (const file of filesToCopy) {
+          const srcFile = join(filesDirPath, file);
+          const destFile = join(filesCopyDirPath, file);
+          await copyFile(srcFile, destFile);
+        }
+      }
+    }
+
+    // if files_copy exists throw an error
+    if (copyExists) {
+      throw new Error('FS operation failed');
+    }
+
+  }
+;
 
 await copy();
