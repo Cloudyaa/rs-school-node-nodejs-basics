bfc2e5eb07ec8978e817a204c22ca07619b4f1d0
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-12 21:23:44 +0100

    Implement create new file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import {writeFile, readFile} from 'node:fs/promises';
+import {join} from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join('files','fresh.txt');
+  let isCreated = false;
+  try {
+    await readFile(filePath);
+    isCreated = true;
+  } catch (e) {
+    if(e.code === 'ENOENT'){
+      await writeFile(filePath, 'I am fresh and young');
+    }
+  }
+
+  if(isCreated){
+    throw new Error('FS operation failed');
+  }
+
 };
 
 await create();
