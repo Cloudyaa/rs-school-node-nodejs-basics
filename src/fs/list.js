f04fbbf905c251676376ade73d8eba34f650a8ab
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 13:50:04 +0100

    Implement function that prints all filenames from files directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import {fileURLToPath} from "url";
+import {dirname, join, parse} from "path";
+import {readdir} from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
-};
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filesDirPath = join(__dirname, 'files');
+
+  try {
+    const files = await readdir(filesDirPath);
+    for (const file of files) {
+      const {name} = parse(file);
+      console.log(name);
+    }
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      throw new Error('Fs operation failed');
+    }
+  }
+}
 
 await list();
