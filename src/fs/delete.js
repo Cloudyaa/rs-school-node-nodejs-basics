10e421ef0af9a76b3cadd602658e445c5cb03257
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 13:20:09 +0100

    Implement function that removes file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import {join} from "path";
+import {readFile, unlink} from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join('src/fs/files','fileToRemove.txt');
+
+  try {
+    await readFile(filePath);
+    await unlink(filePath);
+  } catch (e) {
+      throw new Error('FS operation failed');
+  }
 };
 
 await remove();
