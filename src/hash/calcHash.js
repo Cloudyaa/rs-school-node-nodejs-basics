5c081b45af944e2b3a06e71c34fb17ed1ef515d3
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 15:09:52 +0100

    Implement function that calculates SHA256 hash and logs it into console

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import {createHash} from 'crypto';
+import {createReadStream} from 'fs';
+import {once} from 'events';
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filesDirPath = join(__dirname, 'files');
+  const filePath = join(filesDirPath, 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+
+  const streamToHash = createReadStream(filePath);
+
+  streamToHash.on('readable', () => {
+    const data = streamToHash.read();
+    if (data) {
+      hash.update(data);
+    }
+  });
+
+  await once(streamToHash, 'end');
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
