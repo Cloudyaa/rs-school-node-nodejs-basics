4b90cdb95e1b5e4eca1234775d3583dc31a36858
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 14:00:20 +0100

    implement function that prints content of the file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import {filesDirPath} from "./filesDirPath.js";
+import {join} from "path";
+import {readFile} from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(filesDirPath, 'fileToRead.txt');
+
+  try {
+    const file = await readFile(filePath, 'utf8');
+    console.log(file);
+  } catch (e) {
+    if(e.code === "ENOENT"){
+      throw new Error('Fs operation failed');
+    }
+  }
 };
 
 await read();
