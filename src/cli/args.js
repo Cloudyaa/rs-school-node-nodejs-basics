31c7c4b921d3bfbddfac9e593cf8437f0f078868
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 14:20:20 +0100

    implement function that parses command line arguments and prints them to the console

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+
+  // getting args only (omit "node src/cli/args.js")
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i += 2) {
+    // omit the "--" from arg
+    const propName = args[i].slice(2);
+    const value = args[i + 1];
+    console.log(`${propName} is ${value}`);
+  }
 };
 
 parseArgs();
