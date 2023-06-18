b4b6112ea33fa9c522ed1cae930a251554fc964e
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 13:00:27 +0100

    Implement function that renames file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,36 @@
+import {join} from "path";
+import fs, {readFile} from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFilePath = join('src/fs/files','wrongFilename.txt');
+  const properFilename = join('src/fs/files','properFilename.md');
+  let properExists = false;
+
+  // check if wrongFile exists
+  try {
+    await readFile(wrongFilePath);
+  } catch (e){
+    if(e.code !== "ENOENT"){
+      throw new Error('FS operation failed');
+    }
+  }
+
+  // check if properFile exists
+  try {
+    await readFile(properFilename);
+    properExists = true;
+  } catch (e) {
+    // If properFile doesn't exist, rename wrongFilename to properFilename
+    if(e.code === 'ENOENT'){
+      await fs.rename(wrongFilePath, properFilename);
+    }
+  }
+
+  // throw error if properFile exists already
+  if(properExists){
+    throw new Error('FS operation failed');
+  }
+
 };
 
 await rename();
