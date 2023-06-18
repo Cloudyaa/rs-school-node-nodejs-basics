86f194e0285a4ece45a2d97b1970decbfaff0d56
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-06-18 12:40:00 +0100

    Remove unnecessary comment

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,3 @@
-//implement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
 
 import {copyFile, mkdir, readdir} from 'node:fs/promises';
 import {join, dirname} from 'path';
